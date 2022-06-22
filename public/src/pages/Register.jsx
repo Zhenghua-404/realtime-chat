9ffd03199c563250153877e960879fe222651748
Zhenghua-404 <zhenghua.chen@mail.mcgill.ca>
2022-06-21 23:42:13 -0700

    add icons

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
+import Logo from "../assets/logo.svg";
 function register() {
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -13,7 +13,7 @@ function register() {
       <FormContainer>
         <form on Submit={(event) => handleSubmit(event)}>
           <div className="brand">
-            <img src="" alt="" />
+            <img src={Logo} alt="Logo" />
             <h1>snappy</h1>
           </div>
           <input
@@ -41,12 +41,60 @@ function register() {
             onChange={(e) => handleChange(e)}
           />
           <button type="submit">Create User</button>
-          <span>Already have an account? <Link to="/login">Login</Link></span>
+          <span>
+            Already have an account? <Link to="/login">Login</Link>
+          </span>
         </form>
       </FormContainer>
     </>
   );
 }
 
-const FormContainer = styled.div``;
+
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: lrem;
+  align-items: center;
+  background-color: #131324;
+  .brand{
+    display: flwx;
+    align-items: center;
+    gap: lrem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1{
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      color: white;
+      width: 100%;
+      front-size: 1rem;
+      &:focus {
+        brder: 0.1rem solid #997af0;
+        outline: none;
+      }
+
+    }
+  }
+`;
+
 export default register;
