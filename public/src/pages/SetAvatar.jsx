b1962e5995f85cfd9d5e272593ac9843ec6d55f3
Zhenghua-404 <zhenghua.chen@mail.mcgill.ca>
2022-06-28 22:59:42 -0700

    fixed useEffect error

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import Loader from "../assets/loader.gif";
+import loader from "../assets/loader.gif";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
+import { Buffer } from "buffer";
 import { setAvatarRoute } from "../utils/APIRoutes";
 
 export default function SetAvatar() {
@@ -21,17 +22,33 @@ export default function SetAvatar() {
     theme: "dark",
   };
   const setProfilePicture = async () => {};
-  useEffect(async () => {
-    const data = [];
-    for (let i = 0; i < 4; i++) {
-      const image = await axios.get(
-        `${api}/${Math.round(Math.random() * 1000)}`
-      );
-      const buffer = new Buffer(image.data);
-      data.push(buffer.toString("base64"));
+
+  //   Warning: useEffect must not return anything besides a function, which is used for clean-up.
+  //   It looks like you wrote useEffect(async () => ...) or returned a Promise. Instead, write the async function inside your effect and call it immediately:
+
+  //   useEffect(() => {
+  //     async function fetchData() {
+  //       // You can await here
+  //       const response = await MyAPI.getData(someId);
+  //       // ...
+  //     }
+  //     fetchData();
+  //   }, [someId]); // Or [] if effect doesn't need props or state
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = [];
+      for (let i = 0; i < 4; i++) {
+        const image = await axios.get(
+          `${api}/${Math.round(Math.random() * 1000)}`
+        );
+        const buffer = new Buffer(image.data);
+        data.push(buffer.toString("base64"));
+      }
+      setAvatars(data);
+      setIsLoading(false);
     }
-    setAvatars(data);
-    setIsLoading(false);
+    fetchData();
   }, []);
 
   return (
@@ -49,10 +66,11 @@ export default function SetAvatar() {
                   selectedAvatar === index ? "selected" : ""
                 }`}
               >
+                {/* base64 string as an image */}
                 <img
                   src={`data:image/svg+xml;base64,${avatar}`}
                   alt="avatar"
-                  onClick={() => setSelectedAvatar}
+                  onClick={() => setSelectedAvatar(index)}
                 />
               </div>
             );
